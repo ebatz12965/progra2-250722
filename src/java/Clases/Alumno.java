578749ba329f4bef04f4630e830907b4d312e31c
Edwing Batz <ebatzm@miumg.edu.gt>
2022-07-30 09:15:16 -0600

    Ejercicio en clase del día 25/07/2022

---
+++ src/java/Clases/Alumno.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Clases;
+
+public class Alumno {
+
+    private int codigo;
+    private String nombre;
+
+    public Alumno(){
+        this.nombre="Alumno de ejemplo";
+    }
+    
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
